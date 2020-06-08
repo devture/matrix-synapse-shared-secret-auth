43ccfedb0b862245f354357d97cb112a7e2be87d
Slavi Pantaleev <slavi@devture.com>
2020-06-08 09:23:18 +0300

    Add Twisted to dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="shared_secret_authenticator",
-    version="0.0.1",
+    version="1.0.2",
     py_modules=['shared_secret_authenticator'],
     description="Shared Secret Authenticator password provider module for Matrix Synapse",
     include_package_data=True,
     zip_safe=True,
-    install_requires=[],
+    install_requires=['Twisted'],
 )
 
