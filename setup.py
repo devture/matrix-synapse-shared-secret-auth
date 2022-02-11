6a17618d2f90b23f4ac94d5a9396de404c2281ea
Slavi Pantaleev <slavi@devture.com>
2022-02-11 10:21:17 +0200

    Bump version and mark as Python 3.10-compatible

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shared_secret_authenticator",
-    version="2.0.1",
+    version="2.0.2",
     py_modules=['shared_secret_authenticator'],
     description="Shared Secret Authenticator password provider module for Matrix Synapse",
     include_package_data=True,
@@ -16,6 +16,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
 
